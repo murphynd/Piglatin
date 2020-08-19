99373b6c2b1324b479f95d5416ca20eba6e2df39
Michael Watts <wattsjmichael@gmail.com>
2020-08-19 13:31:15 -0700

    creates initial files and first tests in scripts.js
    Co-Authored-By: Natalie Murphy <nataliemurphy500@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+Test one 
+// target words beginnig with a vowel 
+// add "way" to the end of those words
+
+
+Test two 
+// Target words beginning with consonants, ( include "y" and include "qu") 
+
+//move all of the first consecutive consonants to the end, 
+
+// add "ay" at the end 
+
