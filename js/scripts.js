53c2cf6dffdf2a54405050c05c541b068c574e34
Michael Watts <wattsjmichael@gmail.com>
2020-08-19 14:39:24 -0700

    Complete Test One, adds way to end of words
    Co-Authored-By: Natalie Murphy <nataliemurphy500@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,37 @@
 
-Test one 
+//Test one 
+// add "way" to the end words
+
+const word= ["This"];
+const vowelWord = ["way"];
+const pigLatinWord = word.concat(vowelWord);
+const superPigLatinWord = pigLatinWord.join("");
+
+//Test Two
+////need to splice or slice the first letter of the word
+
+}
 // target words beginnig with a vowel 
-// add "way" to the end of those words
-
-
-Test two 
+//Test two 
 // Target words beginning with consonants, ( include "y" and include "qu") 
 
 //move all of the first consecutive consonants to the end, 
 
 // add "ay" at the end 
 
+
+// console.log(superPigLatinWord);
+// function piglatin(word) {
+// if word[0] === /aeiou/
+//     add + "way" at word.length-1
+
+// function pigLatinvowl(userInput) {
+// 	let vowelEnd = "way"
+// 	let consEnd = "ay"
+// 	let vowels = ["a", "e", "i", "o", "u"];
+//   userInput = [];
+  
+//   array.forEach(function(element){
+//   	Translation.push(element(vowels.charAt(0)+vowelEnd));
+//     });
+//    });
